8cd4639d8f794d835a6025c3c6191eb371f617ae
lukasz789 <lukaszjanyst@student.agh.edu.pl>
2026-08-06 21:59:48 +0200

    add IAM roles for EKS

---
+++ infra/iam.tf
@@ -0,0 +1,67 @@
+# ------------------------------------------------------------------------------
+# IAM role used by the EKS control plane (so IAM role for whole EKS cluster)
+# ------------------------------------------------------------------------------
+# https://docs.aws.amazon.com/eks/latest/userguide/cluster-iam-role.html
+data "aws_iam_policy_document" "eks_cluster_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name               = "${var.project_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
+
+  tags = {
+    Name = "${var.project_name}-cluster-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster" {
+  role = aws_iam_role.eks_cluster.name
+  # managed policy which allows EKS to manage the cluster
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+# ------------------------------------------------------------------------------
+# IAM role used by EC2 instances in the EKS managed node group
+# ------------------------------------------------------------------------------
+# https://docs.aws.amazon.com/eks/latest/userguide/create-node-role.html
+data "aws_iam_policy_document" "eks_nodes_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name               = "${var.project_name}-node-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_nodes_assume_role.json
+
+  tags = {
+    Name = "${var.project_name}-node-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_node" {
+  role = aws_iam_role.eks_nodes.name
+  # managed policy which allows EKS worker nodes to communicate with the control plane
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ecr_pull" {
+  role = aws_iam_role.eks_nodes.name
+  # managed policy which allows EKS worker nodes to pull container images from ECR
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPullOnly"
+}
